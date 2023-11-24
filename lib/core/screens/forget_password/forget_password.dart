0338149105f8f8f7c53840a810656858c581c5cd
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-11-24 23:11:39 +0300

    fix:password reset

--- lib/core/screens/forget_password/forget_password.dart
+++ lib/core/screens/forget_password/forget_password.dart
@@ -6,30 +6,31 @@ import 'package:voice_app/product/repository/auth/auth_repository.dart';
 import 'package:voice_app/product/widgets/buttons/custom_button.dart';
 import 'package:voice_app/product/widgets/textfields/custom_textfield_forget_password.dart';
 
-class ForgetPassword extends StatelessWidget {
+class ForgetPassword extends StatefulWidget {
   const ForgetPassword({super.key});
 
   @override
+  State<ForgetPassword> createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  @override
   Widget build(BuildContext context) {
-    final _controller = TextEditingController();
+    final controller = TextEditingController();
     final TextEditingController emailController = TextEditingController();
 
-    Future<void> passwordReset(
-      String email,
-    ) async {
-      try {
-        // Firebase Authentication'da kullanıcının var olup olmadığını kontrol et
-        var methods =
-            await FirebaseAuth.instance.fetchSignInMethodsForEmail(email);
+    void dispose() {
+      controller.dispose();
 
-        if (methods.isNotEmpty) {
-          await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-          print('Şifre sıfırlama e-postası gönderildi');
-        } else {
-          print('Bu e-posta adresine kayıtlı bir kullanıcı bulunamadı');
-        }
-      } catch (e) {
-        print('Şifre sıfırlama hatası: $e');
+      super.dispose();
+    }
+
+    Future<void> passwordReset() async {
+      try {
+        await FirebaseAuth.instance
+            .sendPasswordResetEmail(email: controller.text.trim());
+      } on FirebaseAuthException catch (e) {
+        print('error: $e');
       }
     }
 
@@ -70,26 +71,24 @@ class ForgetPassword extends StatelessWidget {
             Padding(
               padding: context.padding.low,
               child: CustomTextFieldForgetPassword(
-                controller: _controller,
+                controller: controller,
               ),
             ),
             SizedBox(
               height: context.sized.dynamicHeight(0.02),
             ),
-            TextField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: 'E-posta'),
-                keyboardType: TextInputType.emailAddress,
-                style: context.general.textTheme.titleMedium?.copyWith(
-                  color: ColorConstants.colorsWhite,
-                )),
+            // TextField(
+            //     controller: emailController,
+            //     decoration: InputDecoration(labelText: 'E-posta'),
+            //     keyboardType: TextInputType.emailAddress,
+            //     style: context.general.textTheme.titleMedium?.copyWith(
+            //       color: ColorConstants.colorsWhite,
+            //     )),
             CustomButton(
               description: 'Send Code',
               backgroundColor: ColorConstants.colorBlue,
               onPressed: () {
-                String email = emailController.text;
-
-                passwordReset(email);
+                passwordReset();
               },
             ),
           ],
