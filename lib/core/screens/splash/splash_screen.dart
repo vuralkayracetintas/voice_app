8c05222666ea6aa94b36b1e59039bec13d102014
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-12-02 01:25:18 +0300

    feat: navigate splash to home"

--- lib/core/screens/splash/splash_screen.dart
+++ lib/core/screens/splash/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // navigateToHome();
+    navigateToHome();
   }
 
   navigateToHome() async {
