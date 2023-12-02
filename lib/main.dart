a6a8cfd44c8d22bcd619a46c6a2ee7f4a617a4c6
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-12-02 11:47:17 +0300

    feat: native splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:voice_app/bloc/LoginBloc/login_bloc.dart';
 import 'package:voice_app/bloc/RegisterBloc/register_bloc.dart';
 import 'package:voice_app/bloc/VoiceBloc/voice_bloc.dart';
@@ -23,6 +24,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
